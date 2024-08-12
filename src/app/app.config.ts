bf11938f1fe7fbbf736a04f3511b03b07d3db41b
venusseo127 <venusseo127@gmail.com>
2024-08-12 17:52:16 +0800

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,12 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
+import { provideFirestore, getFirestore, connectFirestoreEmulator } from '@angular/fire/firestore';
+import { provideFunctions, getFunctions, connectFunctionsEmulator} from '@angular/fire/functions';
+import { provideMessaging, getMessaging } from '@angular/fire/messaging';
+import { provideStorage, getStorage, connectStorageEmulator } from '@angular/fire/storage';
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
